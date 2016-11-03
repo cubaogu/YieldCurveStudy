3a831e3e227ef98dce9cf3747d2f0fc4a0043155
chenchen <cubg5@sina.com>
2016-11-03 17:23:01 +0800

    totally refreshed - add some general functions

--- PricingCreditBond.py
+++ PricingCreditBond.py
@@ -43,10 +43,6 @@ compounding = ql.Compounded
 compoundingFrequency = ql.Annual
 
 
-
-
-
-
 def mybond(issueDate, maturityDate, tenor, couponRate,faceValue):
     calendar = ql.UnitedStates()
     bussinessConvention = ql.Unadjusted
@@ -69,14 +65,14 @@ def mybond(issueDate, maturityDate, tenor, couponRate,faceValue):
     return fixedRateBond
     
 
-#寻找个体公司的构Spread:135401.SH，16盘锦水务，10.27号估值106.6656
+#寻找个体公司的Spread:135401.SH，16盘锦水务，10.27号估值106.6656
 issueDate = ql.Date(28, 4, 2016)
 maturityDate = ql.Date(28, 4, 2019)
 tenor = ql.Period(ql.Annual)
 
 fixedRateBond = mybond(issueDate,maturityDate,tenor,0.068,100)
 
-for i in range(1,201):
+for i in range(1,10000):
     spotRates_TMP = [j+ i*0.00005 for j in spotRates]
     #这个schedule构建的时候用tenor之间的间隔算出真实的天数，然后按照dayCount的规则（此处是按每年都365天来算）得到每个rate对应的年份
     spotCurve = ql.ZeroCurve(spotDates, spotRates_TMP, dayCount, calendar, interpolation,compounding, compoundingFrequency) 
@@ -92,50 +88,91 @@ CorpSpread = i* 0.00005 #认为这是公司个体与中债曲线之间的Spread
 
 print(CorpSpread)
     
+
+
+
     
 
-# 寻找市场与估值之间的Spread：1680388.IB,10.27估值100.9709,市场101.1467
-issueDate = ql.Date(28, 4, 2016)
-maturityDate = ql.Date(28, 4, 2019)
-tenor = ql.Period(ql.Annual)
+def price_of_2plus5(issue_day,issue_mon,issue_year,coupon,spotRates): #算出2+5年的债的价格
+    total_price = 0
+    
+    todaysDate = ql.Date(27, 10, 2016) #今天，即定价中的成交日
+    ql.Settings.instance().evaluationDate = todaysDate
+    
+    calendar = ql.UnitedStates()
+    dayCount = ql.Thirty360()
+    interpolation = ql.Linear()
+    compounding = ql.Compounded
+    compoundingFrequency = ql.Annual
+    
+    spotDates = [ql.Date(27, 10, 2016)] + [ql.Date(27, j, i) for i in range(2017,2024) for j in [4,10]]
+    spotCurve = ql.ZeroCurve(spotDates, spotRates, dayCount, calendar, interpolation,compounding, compoundingFrequency) 
+    spotCurveHandle = ql.YieldTermStructureHandle(spotCurve)
+    bondEngine = ql.DiscountingBondEngine(spotCurveHandle)
 
-fixedRateBond = mybond(issueDate,maturityDate,tenor,0.0443,100)
+    for i in range(issue_year+3,issue_year+8):
+        issueDate = ql.Date(issue_day, issue_mon, issue_year)
+        maturityDate = ql.Date(issue_day, issue_mon, i)
+        tenor = ql.Period(ql.Annual)
+        fixedRateBond_t = mybond(issueDate,maturityDate,tenor,coupon,20)
+        fixedRateBond_t.setPricingEngine(bondEngine)
+        total_price += fixedRateBond_t.NPV()
+    return total_price
+
+Market_price_16rzxy_1027 = price_of_2plus5(29,9,2016,0.0443,spotRates)
+
+# 寻找市场与估值之间的Spread：16汝州鑫源1680388.IB,10.27估值100.9708,市场101.1467
 
 for i in range(1,201):
     spotRates_TMP = [j+ i*0.00005 for j in spotRates]
-    #这个schedule构建的时候用tenor之间的间隔算出真实的天数，然后按照dayCount的规则（此处是按每年都365天来算）得到每个rate对应的年份
-    spotCurve = ql.ZeroCurve(spotDates, spotRates_TMP, dayCount, calendar, interpolation,compounding, compoundingFrequency) 
-    spotCurveHandle = ql.YieldTermStructureHandle(spotCurve)
-
-    bondEngine = ql.DiscountingBondEngine(spotCurveHandle)
-    fixedRateBond.setPricingEngine(bondEngine)
-    if abs(fixedRateBond.NPV()-106.6656) < 0.1:
+    if abs(price_of_2plus5(29,9,2016,0.0443,spotRates_TMP)-100.9708) < 0.1:
         break
 
-CorpSpread = i* 0.00005 #认为这是公司个体与中债曲线之间的Spread
+Spread1 = i* 0.00005 #认为这是公司个体与中债曲线之间的Spread
+
+for i in range(1,201):
+    spotRates_TMP = [j+ i*0.00005 for j in spotRates]
+    if abs(price_of_2plus5(29,9,2016,0.0443,spotRates_TMP)-101.1467) < 0.1:
+        break
+
+Spread2 = i* 0.00005 #认为这是市场成交与中债估值之间的Spread
+
+market_spread = Spread2 - Spread1  #认为这是市场成交与中债估值之间的Spread
 
 
-#1680429.IB,16盘锦水务专项，一个2+5的债，10.28发行
-total_price = 0
-
-tommorrowDate= ql.Date(28, 10, 2016) #明天。想估算的债券的发行日
-ql.Settings.instance().evaluationDate = tommorrowDate
-spotDates = [ql.Date(28, 10, 2016)] + [ql.Date(28, j, i) for i in range(2017,2024) for j in [4,10]]
-spotCurve = ql.ZeroCurve(spotDates, spotRates, dayCount, calendar, interpolation,compounding, compoundingFrequency) 
-spotCurveHandle = ql.YieldTermStructureHandle(spotCurve)
-bondEngine = ql.DiscountingBondEngine(spotCurveHandle)
+myspread = CorpSpread + market_spread#我们要定价的债券与中债曲线之间的总利差（个体+市场）
 
 
-for i in range(2019,2024):
-    issueDate = ql.Date(28, 10, 2016)
-    maturityDate = ql.Date(27, 10, i)
-    tenor = ql.Period(ql.Annual)
-    fixedRateBond_t = mybond(issueDate,maturityDate,tenor,0.0518,20)
-    fixedRateBond_t.setPricingEngine(bondEngine)
-    total_price += fixedRateBond_t.NPV()
+#准备定价的债：1680429.IB,16盘锦水务专项，一个2+5的债，10.28发行
+
+spotRates_TMP = [j+myspread for j in spotRates]
+
+for i in range(-200,201):
+    final_price = price_of_2plus5(27,10,2016,i*0.0005 + 0.03,spotRates_TMP)
+    if abs(final_price - 100) <0.1:
+        break
+
+print(i*0.0005+0.03)
+#total_price = 0
+#
+#tommorrowDate= ql.Date(28, 10, 2016) #明天。想估算的债券的发行日
+#ql.Settings.instance().evaluationDate = tommorrowDate
+#spotDates = [ql.Date(28, 10, 2016)] + [ql.Date(28, j, i) for i in range(2017,2024) for j in [4,10]]
+#spotCurve = ql.ZeroCurve(spotDates, spotRates, dayCount, calendar, interpolation,compounding, compoundingFrequency) 
+#spotCurveHandle = ql.YieldTermStructureHandle(spotCurve)
+#bondEngine = ql.DiscountingBondEngine(spotCurveHandle)
+#
+#
+#for i in range(2019,2024):
+#    issueDate = ql.Date(28, 10, 2016)
+#    maturityDate = ql.Date(27, 10, i)
+#    tenor = ql.Period(ql.Annual)
+#    fixedRateBond_t = mybond(issueDate,maturityDate,tenor,0.0518,20)
+#    fixedRateBond_t.setPricingEngine(bondEngine)
+#    total_price += fixedRateBond_t.NPV()
     
-print(total_price)
-    
+
+
 
 
 
